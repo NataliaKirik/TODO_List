5ba2212cd95f332aa30066097551ab5d8f914507
Natalia K <nataliaandkirik@gmail.com>
2021-04-22 15:51:31 +0300

    add: storybook 12

---
+++ src/AddItemForm.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {AddItemForm} from "./AddItemForm";
+// @ts-ignore
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: 'AddItemForm',
+    component: AddItemForm
+
+}
+const callback = action('Button add was pressed inside the form')
+export const AddItemFormExample = () => <AddItemForm addItem={callback}/>
