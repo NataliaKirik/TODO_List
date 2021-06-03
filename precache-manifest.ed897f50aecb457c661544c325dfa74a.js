e3c4129b060bac8b6bb8d49a3c0728dcba615580
Natalia K <nataliaandkirik@gmail.com>
2021-06-03 15:34:51 +0300

    Updates

---
+++ precache-manifest.ed897f50aecb457c661544c325dfa74a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395da3d134be4b07997663ed7b1ad402",
+    "url": "/TODO_List/index.html"
+  },
+  {
+    "revision": "dde2653d99734af2fb65",
+    "url": "/TODO_List/static/css/main.04b8bd3f.chunk.css"
+  },
+  {
+    "revision": "0c4b0354e5f5db4c0952",
+    "url": "/TODO_List/static/js/2.3b34d8b9.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/TODO_List/static/js/2.3b34d8b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde2653d99734af2fb65",
+    "url": "/TODO_List/static/js/main.95a49e50.chunk.js"
+  },
+  {
+    "revision": "d1a42a03a241c5bb0af4",
+    "url": "/TODO_List/static/js/runtime-main.77572426.js"
+  },
+  {
+    "revision": "e322211299c4b2087adb87715ca1d00b",
+    "url": "/TODO_List/static/media/pexels-anni-roenkae-2457284.e3222112.jpg"
+  }
+]);
